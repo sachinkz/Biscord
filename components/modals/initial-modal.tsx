787e3a5adfbba992993510872b59fc56ad47ca60
Sachin K <sachuiritty@gmail.com>
2024-01-05 22:48:55 +0530

    change in useEffect

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -29,7 +29,7 @@ export const InitialModal = () => {
 
     useEffect(() => {
         setIsMounted(true);
-    })
+    },[])
 
     const form = useForm({
         resolver: zodResolver(formSchema),
